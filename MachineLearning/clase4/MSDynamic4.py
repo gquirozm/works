38f59ca558c12d616cf5213570f3b2d9907f4932
javier <javier@192.168.100.41>
2016-10-07 20:33:38 -0500

    # WARNING: head commit changed in the meantime
    segunddo  commit con try/except

--- MachineLearning/clase4/MSDynamic4.py
+++ MachineLearning/clase4/MSDynamic4.py
@@ -1,7 +1,7 @@
 '''
 Created on Oct 6, 2016
 
-@author: javier
+@author: Javier Quiroz
 '''
 import pandas as pd
 import numpy as np
@@ -13,7 +13,7 @@ from sklearn.cross_validation import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import mean_squared_error
 from sklearn import preprocessing
-from scipy.stats import norm
+#from scipy.stats import norm
 from mpmath.tests.test_quad import xtest_double_7
 from dask.array.core import asarray
 from numba.tests.test_array_methods import np_around_array
@@ -43,8 +43,8 @@ class Adjuster:
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(df[df.columns[0:-1]],df[df.columns[-1]], train_size=0.75, random_state = 0)
 
         self.x_train['X0']  = 1
-        self.xs = self.x_train[ ['X0', 'X'] ][0:750] # por momento solo un conjunto pequeno de datos 
-        self.ys = self.y_train[0:750]
+        self.xs = self.x_train[ ['X0', 'X'] ]# [0:750] # por momento solo un conjunto pequeno de datos 
+        self.ys = self.y_train# [0:750]
         self.xs.reset_index(drop=True, inplace=True); #ojo con los indices que no llegan numerados de 0 a length-1
         self.ys.reset_index(drop=True, inplace=True);
 
@@ -61,15 +61,15 @@ class Adjuster:
         i = 0 # indexa renglones
     
         while ( i < self.num_rows):
-            t_last_w = pd.Series(self.last_w)
-            #print self.xs.iloc[i]
-            #print t_last_w
-            VxX = np.dot( self.xs.iloc[i] , t_last_w)
-            #print "printing ys"%(self.ys)
-            last_e = float( self.ys[i] - VxX  )
-            for  j in range(self.num_cols):
-                self.last_w[j] = float ( self.last_w[j] + self.n * last_e * self.xs.iloc[i,j] )
+            try:
+                t_last_w = pd.Series(self.last_w)
+                VxX = np.dot( self.xs.iloc[i] , t_last_w)
+                last_e = float( self.ys[i] - VxX  )
+                for  j in range(self.num_cols):
+                    self.last_w[j] = float ( self.last_w[j] + self.n * last_e * self.xs.iloc[i,j] )
                 i  = i + 1
+            except :
+                print "Indices i = %i ,j= %i"%(i,j)
         self.w0.append(self.last_w[0])
         self.w1.append(self.last_w[1])
         temp_rmse = self.calculate_rmse()
@@ -81,7 +81,6 @@ class Adjuster:
     def calculate_rmse(self):
         y_temp = self.last_w[0] * self.xs["X0"] + self.last_w[1] * self.xs["X"]
         RMSE = mean_squared_error(self.ys, y_temp)**0.5
-     
         return  RMSE
 
 
@@ -121,12 +120,13 @@ class Adjuster:
 #===========================================================================
 print "<Start of Program>"
 
-myAdjuster = Adjuster(eta = 0.0179)
+#myAdjuster = Adjuster(eta = 0.0179)
+myAdjuster = Adjuster(eta = 0.00475)
 myAdjuster.read_data()
 #myAdjuster.print_xs()
 
 
-for i in range(10):    
+for i in range(40):    
     print "Ciclo = ", i
     myAdjuster.learn()
 
